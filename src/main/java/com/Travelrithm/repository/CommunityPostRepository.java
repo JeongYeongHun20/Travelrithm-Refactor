cc227a1b2d325ab145e8009e36728a3c9804650f
mj0002 <dbalswjd0223@naver.com>
2025-05-17 22:23:49 +0900

    인기 플랜 JPQL

--- src/main/java/com/Travelrithm/repository/CommunityPostRepository.java
+++ src/main/java/com/Travelrithm/repository/CommunityPostRepository.java
@@ -1,14 +1,31 @@
 package com.Travelrithm.repository;
 
 import com.Travelrithm.domain.CommunityPostEntity;
+import com.Travelrithm.domain.PlanEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommunityPostRepository extends JpaRepository<CommunityPostEntity, Integer> {
+
     List<CommunityPostEntity> findAllByUserEntity_UserId(Integer userId);
+
     Page<CommunityPostEntity> findAllByIsTravelPlanTrue(Pageable pageable);   // 플랜게시판
     Page<CommunityPostEntity> findAllByIsTravelPlanFalse(Pageable pageable);  // 자유게시판
+
+    // 인기 plan 조회 (region 기준)
+    @Query("""
+        SELECT p
+        FROM CommunityPostEntity c
+        JOIN c.planEntity p
+        WHERE p.regionEntity.regionId = :regionId
+        GROUP BY p
+        ORDER BY COUNT(c) DESC
+    """)
+    List<PlanEntity> findTopPopularPlanByRegion(@Param("regionId") Integer regionId, Pageable pageable);
 }
+
