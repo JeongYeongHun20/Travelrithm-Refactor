3f854b1cbca6010f1fdfdcc200d76abffa8a276c
JeongYeongHun <hotpizza0918@naver.com>
2025-05-17 18:05:35 +0900

    S3 upload donwload 완료

---
+++ src/main/java/com/Travelrithm/s3/S3Controller.java
@@ -0,0 +1,27 @@
+package com.Travelrithm.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+
+
+    private final S3Service s3Service;
+
+    @GetMapping("/upload-url")
+        public Map<String, String> getUploadUrl(@RequestParam String filename) {
+        return s3Service.generatePreSignedPutUrl(filename);
+    }
+
+    @GetMapping("/download-url")
+    public String getDownloadUrl(@RequestParam String key) {
+        return s3Service.generatePreSignedGetUrl(key);
+    }
+}
