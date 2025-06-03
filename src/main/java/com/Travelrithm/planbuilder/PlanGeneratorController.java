6a44259af38f5258ebea810a4167a7446ffc75f3
JeongYeongHun <hotpizza0918@naver.com>
2025-06-04 00:52:16 +0900

    totalResponseDto에 간단한 설명 추가

--- src/main/java/com/Travelrithm/planbuilder/PlanGeneratorController.java
+++ src/main/java/com/Travelrithm/planbuilder/PlanGeneratorController.java
@@ -1,21 +1,15 @@
 package com.Travelrithm.planbuilder;
 
 
-import com.Travelrithm.planbuilder.dto.front.DayMap;
 import com.Travelrithm.planbuilder.dto.front.EditPlanner;
-import com.Travelrithm.planbuilder.dto.front.Location;
 import com.Travelrithm.planbuilder.dto.publicdata.TotalResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/geneartor")
+@RequestMapping("/generator")
 @CrossOrigin(origins = "http://localhost:3000")
 public class PlanGeneratorController {
 
--- src/main/java/com/Travelrithm/planbuilder/dto/publicdata/TotalResponseDto.java
+++ src/main/java/com/Travelrithm/planbuilder/dto/publicdata/TotalResponseDto.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 public record TotalResponseDto(
-        List<CommonResponseDto> commonResponseDtos,
-        Map<String, List<DayMap.Content>> result,
-        List<WayPointResponseDto> wayPoints
+        List<CommonResponseDto> commonResponseDtos, //카테고리별 장소들 설명, 및 이미지 파일 차후에 map으로 바꿔서 key값으로 찾게하려고
+        Map<String, List<DayMap.Content>> result, //front에서 받은 장소들 greedy알고리즘 사용해서 나온 순서대로 리턴
+        List<WayPointResponseDto> wayPoints //위 장소들 순서대로 경로값 설정
 ) {
 }
