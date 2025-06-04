e464f54f81233773b85ebd4a7ac5bd43f93c3eb7
JeongYeongHun <hotpizza0918@naver.com>
2025-06-04 09:48:15 +0900

    주석문 추가

--- src/main/java/com/Travelrithm/planbuilder/dto/front/EditPlanner.java
+++ src/main/java/com/Travelrithm/planbuilder/dto/front/EditPlanner.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public record EditPlanner(
         List<DayMap> dayMapList,
-        String travelDestination,
-        String preference,
-        String fatigue,
-        TransportMode transportMode
+        String travelDestination, //여행갈 지역 ex(서울, 광주 등)
+        String preference, //관광 쥐향(자연, 문화, 액티비티)
+        String fatigue,// 많이 돌아다니는가, 적게 돌아다니는가(관광형, 휴양형)
+        TransportMode transportMode //대중고툥 or bus
 ) {
 }
