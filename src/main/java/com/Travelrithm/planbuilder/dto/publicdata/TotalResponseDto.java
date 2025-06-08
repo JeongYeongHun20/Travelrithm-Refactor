15d244f37f8cb05467eeefb8961e3887149c2345
JeongYeongHun <hotpizza0918@naver.com>
2025-06-08 18:16:06 +0900

    bus route 추가

--- src/main/java/com/Travelrithm/planbuilder/dto/publicdata/TotalResponseDto.java
+++ src/main/java/com/Travelrithm/planbuilder/dto/publicdata/TotalResponseDto.java
@@ -2,6 +2,7 @@ package com.Travelrithm.planbuilder.dto.publicdata;
 
 import com.Travelrithm.planbuilder.dto.front.DayMap;
 import com.Travelrithm.planbuilder.dto.kakao.mobility.WayPointResponseDto;
+import com.Travelrithm.planbuilder.dto.tmap.TmapPathResponseDto;
 
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 public record TotalResponseDto(
         Map<Integer, Map<String, List<Item>>> commonResponseDtos, //카테고리별 장소들 설명, 및 이미지 파일 차후에 map으로 바꿔서 key값으로 찾게하려고
         Map<Integer, List<DayMap.Content>> result, //front에서 받은 장소들 greedy알고리즘 사용해서 나온 순서대로 리턴
-        List<WayPointResponseDto> wayPoints //위 장소들 순서대로 경로값 설정
+        List<WayPointResponseDto> wayPoints, //위 장소들 순서대로 경로값 설정
+        Map<Integer, List<TmapPathResponseDto>> busPoints
 ) {
 }
