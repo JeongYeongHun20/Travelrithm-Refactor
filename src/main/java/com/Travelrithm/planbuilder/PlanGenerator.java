50a6380fafa4aef11a1459c128debdb86f5c3919
JeongYeongHun <hotpizza0918@naver.com>
2025-06-09 16:56:20 +0900

    수정본

--- src/main/java/com/Travelrithm/planbuilder/PlanGenerator.java
+++ src/main/java/com/Travelrithm/planbuilder/PlanGenerator.java
@@ -345,9 +345,9 @@ public class PlanGenerator {
         }
         CompleteResponseDto completeResponseDto;
         if(transportMode==TransportMode.car)
-            completeResponseDto = new CompleteResponseDto(wayPointResponseDtos,null , completePlaces);
+            completeResponseDto = new CompleteResponseDto(wayPointResponseDtos,List.of() , completePlaces);
         else
-            completeResponseDto = new CompleteResponseDto(null, busPointResponsDtos , completePlaces);
+            completeResponseDto = new CompleteResponseDto(List.of(), busPointResponsDtos , completePlaces);
 
 
         return completeResponseDto;
