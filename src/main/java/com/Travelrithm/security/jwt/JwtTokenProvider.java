1ab1c62f4252ff061bf74a29caef480e741c45ea
JeongYeongHun <hotpizza0918@naver.com>
2025-06-12 13:32:52 +0900

    key 오류 수정

--- src/main/java/com/Travelrithm/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/Travelrithm/security/jwt/JwtTokenProvider.java
@@ -3,6 +3,7 @@ package com.Travelrithm.security.jwt;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
@@ -10,7 +11,8 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private final String secretKey = "verySecretKeyThatShouldBeLongEnoughForHS256Algorithm12345"; // 최소 256비트 (32바이트)
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
     private Key key;
     private final long tokenValidityInMilliseconds = 1000L * 60 * 60 * 24; // 24시간
 
@@ -18,8 +20,6 @@ public class JwtTokenProvider {
     protected void init() {
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
     }
-
-    // ✅ 토큰 생성
     public String generateToken(String userId) {
         Date now = new Date();
         Date expiry = new Date(now.getTime() + tokenValidityInMilliseconds);
