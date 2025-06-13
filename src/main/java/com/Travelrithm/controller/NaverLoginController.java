f8c3b398e006ab39bdf9da2e3715f492524f5e3d
JeongYeongHun <hotpizza0918@naver.com>
2025-06-13 17:35:35 +0900

    naverLogin 수정

--- src/main/java/com/Travelrithm/controller/NaverLoginController.java
+++ src/main/java/com/Travelrithm/controller/NaverLoginController.java
@@ -2,6 +2,7 @@ package com.Travelrithm.controller;
 
 import com.Travelrithm.dto.NaverUserResponseDto;
 import com.Travelrithm.dto.UserResponseDto;
+import com.Travelrithm.security.jwt.JWTUtil;
 import com.Travelrithm.service.NaverLoginService;
 import com.Travelrithm.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import java.util.Map;
 public class NaverLoginController {
     private final NaverLoginService naverLoginService;
     private final UserService userService;
+    private final JWTUtil jwtUtil;
 
     @GetMapping("/login")
     public ResponseEntity<Map<String, String>> loginPage() {
@@ -34,8 +36,8 @@ public class NaverLoginController {
         String accessToken = naverLoginService.getAccessToken(code);
         NaverUserResponseDto userInfo = naverLoginService.getUserInfo(accessToken);
         UserResponseDto userDto = userService.createUser(userInfo);
-        String redirectUrl = "https://travelrithm.kro.kr/Main?token=" + "jwtToken";
-
+        String jwtToken = jwtUtil.createJwt(userDto.userId(),userDto.email(),"user",24*60*60*1000L); // 또는 userDto.getEmail() 등
+        String redirectUrl = "https://travelrithm.kro.kr/Main?token=" + jwtToken;
         return new RedirectView(redirectUrl);
     }
 }
